dec6932646d15eed6963e3fc14398d5c5dcb2f62
Tommy Sheu <tommy.sheu@gmail.com>
2013-07-10 02:24:23 +0000

    Updated web.js: read from file

--- web.js
+++ web.js
@@ -2,8 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  buf = fs.readFileSync('index.html');
+  response.send(buf.toString();
+  //response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
